06f26b239960058cd15d42def29fd1047c357fab
SV <scottyv6@gmail.com>
2022-06-28 15:35:58 +0800

    Finalize the hompage views

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from app.routes import home, dashboard
 from app.routes import home
 from flask import Flask
+from app.db import init_db
+from app.utils import filters
 
 def create_app(test_config=None):
   # set up app config
@@ -17,5 +19,12 @@ def create_app(test_config=None):
   # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+
+  # set up filters
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
+   
+  init_db(app)
 
   return app
